05c14cca59b761ecadc4e6d38ff472f1f03b1f2b
Naphat <naphat.d@gmail.com>
2024-01-29 12:33:15 +0700

    map page feature

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,7 +3,7 @@ import express, { Request, Response } from "express";
 const router = express.Router();
 
 router.get("/", (_: Request, res: Response) => {
-  return res.status(200).json({ version: "v1.0.0" });
+  return res.status(200).json({ version: "v1.0.1" });
 });
 
 
